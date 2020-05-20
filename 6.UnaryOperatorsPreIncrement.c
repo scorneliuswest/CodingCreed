b15952654a354497f8504e55664b2c8856d654b9
scorneliuswest <64051856+scorneliuswest@users.noreply.github.com>
2020-05-20 15:51:26 -0500

    Add files via upload

---
+++ 6.UnaryOperatorsPreIncrement.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+/***************************************************
+Author : Steven West
+Purpose : To learn about unary operators
+*************************************************/
+void main ()
+{
+    int a=5;
+    int b;
+    b=++a;
+    printf("a=%d , b=%d",a,b);
+
+    getch();
+}
