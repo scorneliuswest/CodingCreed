a65d1340fc5826a7d20c92b63b8dcbbfb15604f6
scorneliuswest <64051856+scorneliuswest@users.noreply.github.com>
2020-05-26 17:03:29 -0500

    Add files via upload

---
+++ 12.SumOf10Numbers(1+2+3+4+5+6+7+8+9+10).c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int sum =0, i=1;
+    while(i<=10)
+    {
+
+    sum=sum+i;
+    i++;
+    }
+    printf("Sum of Numbers : %d",sum);
+    getch();
+}
