d3fe854d1b09ec2a7be9b6c0f7d4fba7563c72b8
scorneliuswest <64051856+scorneliuswest@users.noreply.github.com>
2020-05-26 14:25:02 -0500

    Add files via upload

---
+++ 11.Hello5TimesForLoop5x1-10NextLine.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+//************************************************************************
+//Multiplication Table
+
+
+void main()
+{
+    int i,n=5;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d x %d = %d\n",n,i,n*i);
+    }
+
+    getch();
+}
