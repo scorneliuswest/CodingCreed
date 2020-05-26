48973e4eae449356d7a27f83007ba0f563d6e54d
scorneliuswest <64051856+scorneliuswest@users.noreply.github.com>
2020-05-26 13:55:04 -0500

    Add files via upload

---
+++ 11.Hello5TimesLoops.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("\nhello");
+    printf("\nhello");
+    printf("\nhello");
+    printf("\nhello");
+    printf("\nhello");
+    getch();
+}
