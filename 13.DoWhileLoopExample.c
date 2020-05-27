00032493e22fc8cd0790e8128413ecc1fcd33ef4
scorneliuswest <64051856+scorneliuswest@users.noreply.github.com>
2020-05-26 19:04:14 -0500

    Add files via upload

---
+++ 13.DoWhileLoopExample.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+/***********************************************************************
+Author : Steven West
+Purpose : To add two numbers and ask to continue or not using
+          do while loop
+/**********************************************************************/
+void main()
+{
+    int i=1;
+    do
+    {
+        printf("Hello");
+        i++;
+    }
+    while(i<=5);
+    getch();
+
+}
+
